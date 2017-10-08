2dfd2d1e3d2ca45da46330f31823a2a15b865782
jayant sarkar <codejayant@gmail.com>
2017-10-08 07:06:35 -0700

    reading property from http response body and printing it

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600%20Amphitheatre%20Parkway%20santa%20clara',
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(response, undefined, 2))
+  console.log(`Address : ${body.results[0].formatted_address}`)
+  console.log(`Latitude : ${body.results[0].geometry.location.lat}`)
+  console.log(`Longitude : ${body.results[0].geometry.location.lng}`)
 })
