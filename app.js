aed5854454ebee8200c2a3f8028ea3740f2c1325
jayant sarkar <codejayant@gmail.com>
2017-10-08 06:53:56 -0700

    checking response from the api call

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600%20Amphitheatre%20Parkway%20santa%20clara',
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2))
+  console.log(JSON.stringify(response, undefined, 2))
 })
