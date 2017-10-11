f335b4318231578afcc6b6b1debdb4647344b3c8
Jayant Sarkar <codejayant@gmail.com>
2017-10-10 19:52:57 -0700

    parameterised promises

--- playground/promise.js
+++ playground/promise.js
@@ -1,3 +1,21 @@
+var asyncAdd = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof a === 'number' && typeof b === 'number') {
+        resolve(a + b)
+      } else {
+        reject('Arguments must be number')
+      }
+    }, 1500)
+  })
+}
+
+asyncAdd(5, 7).then((res) => {
+  console.log('Results: ' + res)
+}, (errorMessage) => {
+  console.log(errorMessage)
+})
+
 var somePromise = new Promise((resolve, reject) => {
   setTimeout(() => {
     // either resolve or reject and not both
